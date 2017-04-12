a236182bcabf95d90809d66eff4cff3e76f84ba9
Michael Comella <michael.l.comella@gmail.com>
2017-04-11 17:44:14 -0700

    example: add module to project.

---
+++ example/src/main/java/org/mozilla/accountsexample/AccountsExampleActivity.java
@@ -0,0 +1,13 @@
+package org.mozilla.accountsexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountsExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
