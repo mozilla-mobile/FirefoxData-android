09a3063190bc62590f1ef0e52a214232c8c91c49
Michael Comella <michael.l.comella@gmail.com>
2017-05-09 17:14:40 -0700

    Add FirefoxAccountUtils.advanceAccountToMarried.

--- accounts/src/main/java/org/mozilla/sync/FirefoxAccountUtils.java
+++ accounts/src/main/java/org/mozilla/sync/FirefoxAccountUtils.java
@@ -4,9 +4,18 @@
 
 package org.mozilla.sync;
 
+import android.support.annotation.WorkerThread;
+import org.mozilla.gecko.background.fxa.FxAccountClient;
+import org.mozilla.gecko.browserid.BrowserIDKeyPair;
+import org.mozilla.gecko.fxa.login.FxAccountLoginStateMachine;
+import org.mozilla.gecko.fxa.login.FxAccountLoginTransition;
 import org.mozilla.gecko.fxa.login.Married;
 import org.mozilla.gecko.fxa.login.State;
 import org.mozilla.gecko.fxa.login.State.StateLabel;
+import org.mozilla.sync.login.FirefoxAccountLoginDefaultDelegate;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.Executor;
 
 /**
  * A collection of functions for operating on a FirefoxAccount.
@@ -29,5 +38,28 @@ public class FirefoxAccountUtils {
             throw new IllegalArgumentException("Expected account to be married.");
         }
         return (Married) accountState;
+    }
+
+    public static void advanceAccountToMarried(final FirefoxAccount firefoxAccount, final Executor backgroundExecutor, final MarriedLoginCallback callback) {
+        backgroundExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                new FxAccountLoginStateMachine().advance(firefoxAccount.accountState, StateLabel.Married, new FirefoxAccountLoginDefaultDelegate(firefoxAccount, backgroundExecutor) {
+                    @Override
+                    public void handleFinal(final State state) {
+                        if (isMarried(state)) {
+                            callback.onMarried(getMarried(state));
+                        } else {
+                            callback.onNotMarried(state);
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    interface MarriedLoginCallback {
+        void onMarried(Married marriedState);
+        void onNotMarried(State notMarriedState);
     }
 }
