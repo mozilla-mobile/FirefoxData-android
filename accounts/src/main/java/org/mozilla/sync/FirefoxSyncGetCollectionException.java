f97219bb4a46aa22f6b266a2441c2e3242b17d25
Michael Comella <michael.l.comella@gmail.com>
2017-05-15 17:35:14 -0700

    Clean up GetCollectionException.

--- accounts/src/main/java/org/mozilla/sync/FirefoxSyncGetCollectionException.java
+++ accounts/src/main/java/org/mozilla/sync/FirefoxSyncGetCollectionException.java
@@ -5,19 +5,36 @@
 package org.mozilla.sync;
 
 /**
- * TODO: name?
+ * A Exception that occurs while a user tries to get data located in one of their Sync collections,
+ * like history or bookmarks.
+ *
+ * Note that the exceptions returned by {@link #getCause()} may include user info like email addresses
+ * so be careful how you log them.
+ * todo: ^ do we want to strip the stuff ourselves?
  */
 public class FirefoxSyncGetCollectionException extends Exception {
     private final FailureReason failureReason;
+
+    public FirefoxSyncGetCollectionException(final String message, final FailureReason failureReason) {
+        super(message + ". " + failureReason.toString());
+        this.failureReason = failureReason;
+    }
 
     public FirefoxSyncGetCollectionException(final Throwable cause, final FailureReason failureReason) {
         super(failureReason.toString(), cause);
         this.failureReason = failureReason;
     }
 
+    /**
+     * Gets the reason this exception was thrown. Normally, this would be handled by the type of the exception thrown
+     * but because these Exceptions are not thrown but passed by callback, this is used instead.
+     *
+     * @return the reason this exception was thrown.
+     */
     public FailureReason getFailureReason() { return failureReason; }
 
-    public enum FailureReason { // TODO: To RecommendedAction?
+    // Some reasons: https://github.com/mozilla/fxa-auth-server/blob/master/docs/api.md#response-format
+    public enum FailureReason {
         REQUIRES_LOGIN_PROMPT, // TODO: should we be more specific? less specific so multiple ways to handle? Return more data?
         UNKNOWN,
     }
